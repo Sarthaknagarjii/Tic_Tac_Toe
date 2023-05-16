c872a4117e7b608ccbefe66664c9ad1ef1a3623c
sarthaknagarjii <sarthaknagarjii@gmail.com>
2023-05-17 00:08:08 +0530

    add cross by clicking

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     public void load(View view){
+
+        ImageView v=(ImageView) view;
+        v.setImageResource(R.drawable.cross);
+
 
     }
 
